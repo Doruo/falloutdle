a66f35435045071f36006d93aa60d234505d5fcb
Marc Haye <marc.antonio.haye@gmail.com>
2025-07-18 11:38:15 +0200

    refactor: comm content_show_length wiki_test

--- tests/wiki_test.go
+++ tests/wiki_test.go
@@ -9,8 +9,7 @@ import (
 
 var wiki_client = wiki.NewWikiClient()
 var character_name = "Roger_Maxson"
-var content_show_length = 2000
-
+var content_show_length = 0 // Set value > 0 to display response content
 func TestMediaWikiClient_GetPageContent(t *testing.T) {
 
 	if _, err := wiki_client.GetPageContent(character_name); err != nil {
