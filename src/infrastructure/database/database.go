3bf053478b6a4860a4557cf8974a7863e7ead7ce
Marc Haye <marc.antonio.haye@gmail.com>
2025-07-22 14:38:37 +0200

    feat: database.go with setup

---
+++ src/infrastructure/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+
+	"github.com/doruo/falloutdle/src/domains/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func SetupDatabase() *gorm.DB {
+	// Database
+	dsn := "host=localhost user=username password=password dbname=falloutdle port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // Logs SQL (dev)
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Auto-migration
+	err = db.AutoMigrate(&models.Character{})
+	if err != nil {
+		log.Fatal("Failed to migrate:", err)
+	}
+
+	return db
+}
