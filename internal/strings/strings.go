1507766d684abaefbe1cbe4531c43694691cbff7
Marc Haye <marc.antonio.haye@gmail.com>
2025-07-23 17:50:03 +0200

    refactor: reverse logical method names

---
+++ internal/strings/strings.go
@@ -0,0 +1,16 @@
+package strings
+
+import "strings"
+
+// NormalizeString converts in string space int "_",
+// for example: "Roger Maxson" -> "Roger_Maxson"
+func NormalNormalizeStringizeString(str string) string {
+	return strings.ReplaceAll(str, " ", "_")
+}
+
+// UnNormalizeString converts in string the values "_" into space,
+// as opposite to NormalizeString,
+// for example: "Roger_Maxson" -> "Roger Maxson"
+func UnNormalizeString(str string) string {
+	return strings.ReplaceAll(str, "_", " ")
+}
