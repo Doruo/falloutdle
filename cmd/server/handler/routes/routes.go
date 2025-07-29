27b3d6dc27e0da11b75991cbd310f50775315c77
Marc Haye <marc.antonio.haye@gmail.com>
2025-07-29 11:17:28 +0200

    feat: routes var

--- cmd/server/handler/routes/routes.go
+++ cmd/server/handler/routes/routes.go
@@ -6,10 +6,21 @@ import (
 	"github.com/doruo/falloutdle/cmd/server/handler"
 )
 
+// GET routes
+const routeGetHome = "/"
+const routeGetTodayCharacter = "/api/today"
+const routeGetRandomCharacter = "/api/random"
+
+// POST routes
+const routePostGuess = "/api/guess"
+
 func SetupRoutes(mux *http.ServeMux, handler *handler.GameHandler) {
 
-	mux.HandleFunc("/", handler.HandleGetHome)
-	mux.HandleFunc("/api/today", handler.HandleGetTodayCharacter)
-	mux.HandleFunc("/api/random", handler.HandleGetRandomCharacter)
-	mux.HandleFunc("/api/guess", handler.HandlePostGuessCharacter)
+	// GET routes
+	mux.HandleFunc(routeGetHome, handler.HandleGetHome)
+	mux.HandleFunc(routeGetTodayCharacter, handler.HandleGetTodayCharacter)
+	mux.HandleFunc(routeGetRandomCharacter, handler.HandleGetRandomCharacter)
+
+	// POST routes
+	mux.HandleFunc(routePostGuess, handler.HandlePostGuessCharacter)
 }
